ceddb57e532deaf03e5464cebac2d31afc1a830b
Sushantshrestha1111 <shresthasushant11@gmail.com>
2023-09-18 19:25:23 +0545

    commited

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,114 @@
 import styled from "styled-components";
+import Post from "./PostCard";
 const Main=(props)=>{
-    return <Container>Main</Container>
+    return <Container>
+        <ShareBox>
+            <Profile>
+            <img src="/images/my.jpg" alt="" />
+
+              <input type="text" placeholder="Write somthing"/>
+
+            </Profile>
+            <Second>
+            <Postoption>
+            <img src="/images/my.jpg" alt="" />
+            <span>Media</span>
+            </Postoption>
+            <Postoption>
+            <img src="/images/my.jpg" alt="" />
+            <span>photo</span>
+            </Postoption>
+            <Postoption>
+            <img src="/images/my.jpg" alt="" />
+            <span>new artical</span>
+            </Postoption>
+            </Second>
+
+          
+            
+       
+        </ShareBox>
+        <Post/>
+        <Post/>
+        
+    </Container>
 }
 export default Main;
 const Container=styled.div`
 
+`
+const CommonCard=styled.div`
+text-align: center;
+overflow: hidden;
+margin-bottom: 8px;
+background-color: #fff;
+border-radius: 5px;
+
+position: relative;
+border: none;
+box-shadow:0 0 0 1px rgb(0 0 0/15%),0 0 0 rgb(0 0 0/20%);
+`
+const ShareBox=styled(CommonCard)`
+display:flex;
+flex-direction:column;
+color:#958b7b;
+margin:0 0 8px;
+background: white;
+
+`
+const Profile=styled.div`
+display:flex;
+
+img{
+    margin-top: 10px;
+    margin-left: 10px;
+    width:100%;
+    height:100%;
+
+    width:46px;height:46px;
+    border-radius: 50%;
+}
+input{
+    border-radius: 25px;
+    padding:15px;
+    margin-top: 10px;
+   width:80%;
+    margin-left: 15px;
+    box-shadow: none;
+     
+        font-weight: 400;
+        font-size: 14px;
+       
+        border-color: #dce6f1;
+   
+}
+
+`
+const Second=styled.div`
+display: flex;
+
+padding-bottom: 10px;
+margin-left: 50px;
+`
+const Postoption=styled.div`
+cursor: pointer;
+margin-top: 8px;
+padding:10px;
+border-radius: 10px;
+
+display:flex;&:hover{
+    background-color: #DCDCDC;
+}
+
+img{
+    width:30px ;
+    margin-left: 20px;
+    
+
+}
+span{
+    margin-left: 25px;
+    margin-right:50px ;
+    
+}
 `
---
+++ src/Components/PostCard.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Post() {
+  return (
+    <PostContainer>
+      <Detail>
+        <img src="/images/my.jpg" alt="" />
+        <Name>
+          <span>Name</span>
+          <p>details</p>
+          <p>time</p>
+        </Name>
+      </Detail>
+      <Description>Here goes the description about the post</Description>
+      <SharedImage>
+        <a>
+          <img src="/images/miamido1.jpg" alt="" />
+        </a>
+      </SharedImage>
+      <ReactionButtons>
+        <button>Like</button>
+        <button>Comment</button>
+        <button>Share</button>
+      </ReactionButtons>
+      <CommentSection>
+        
+        {/* Add comment components here */}
+      </CommentSection>
+    </PostContainer>
+  );
+}
+
+export default Post;
+
+const Name = styled.div`
+  padding-top: 10px;
+  padding-left: 10px;
+  span {
+    color: black;
+    font-size: 30px;
+    font-style: italic;
+  }
+`;
+
+const PostContainer = styled.div`
+  display: flex;
+  border-color: black;
+  border:solid;
+  box-shadow:0 0 0 1px rgb(0 0 0/15%),0 0 0 rgb(0 0 0/20%);
+  border:12px;
+  flex-direction: column;
+  color: #958b7b;
+  margin-top: 15px;
+  margin-bottom:15px;
+  margin: 0 0 8px;
+  background: white;
+  img {
+    width: 100%;
+    max-height: 300px;
+    object-fit: contain;
+  }
+`;
+
+const Detail = styled.div`
+  display: flex;
+  flex-direction: row;
+  img {
+    margin-top: 10px;
+    margin-left: 10px;
+    width: 46px;
+    height: 46px;
+    border-radius: 50%;
+  }
+`;
+const Description=styled.div`
+margin-top:10px;
+margin-left:15px;
+color:black;
+margin-bottom: 10px;
+`
+
+const SharedImage = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+ 
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    max-height: 500px;
+  }
+`;
+
+const ReactionButtons = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 8px;
+  padding-bottom:15px;
+  button {
+    padding: 8px 16px;
+    border: none;
+    background-color: #f0f0f0;
+    cursor: pointer;
+  }
+`;
+
+const CommentSection = styled.div`
+  /* Style your comment section as needed */
+`;
